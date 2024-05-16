5ddf0e63f9ec18b42f6720279868aff7a5bd517e
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-05-16 23:53:12 +0530

    Portfolio-Section

--- src/Pages/Dashboard/Components/PortfolioSection.jsx
+++ src/Pages/Dashboard/Components/PortfolioSection.jsx
@@ -1,18 +1,39 @@
-import { HStack, Icon, Stack, Text } from "@chakra-ui/react";
+import { Button, HStack, Icon, Stack, Tag, Text } from "@chakra-ui/react";
 import { AiOutlineInfoCircle } from "react-icons/ai";
+// import { PiHandDepositDuotone } from "react-icons/pi";
+// import { PiHandWithdrawDuotone } from "react-icons/pi";
+import { PiHandWithdrawFill } from "react-icons/pi";
+import { PiHandDepositFill } from "react-icons/pi";
 
 const PortfolioSection = () => {
   return (
-    <HStack bg="white" border="xl" p="6">
-      <Stack>
-        <HStack color="black.80">
-          <Text fontSize="14px" >Total Portfolio Value</Text>
-          <Icon as={AiOutlineInfoCircle} />
-        </HStack>
-        <Text fontSize="24px">
-        ₹ 114,525.53
-        </Text>
-      </Stack>
+    <HStack bg="white" border="xl" p="6" spacing={16} justify={"space-between"}>
+      <HStack>
+        <Stack>
+          <HStack color="black.80">
+            <Text fontSize="sm">Total Portfolio Value</Text>
+            <Icon as={AiOutlineInfoCircle} />
+          </HStack>
+          <Text textStyle="h2">₹ 114,525.53</Text>
+        </Stack>
+        <Stack>
+          <HStack color="black.80">
+            <Text fontSize="sm">Wallet Balances</Text>
+          </HStack>
+          <HStack>
+            <HStack>
+              <Text textStyle="h2">22.39047815</Text> <Tag>BTC</Tag>
+            </HStack>
+            <HStack>
+              <Text textStyle="h2">₹ 1,300.00</Text> <Tag>INR</Tag>
+            </HStack>
+          </HStack>
+        </Stack>
+      </HStack>
+      <HStack>
+        <Button leftIcon={<Icon as={PiHandDepositFill} />}>Deposit</Button>
+        <Button leftIcon={<Icon as={PiHandWithdrawFill} />}>Withdrawl</Button>
+      </HStack>
     </HStack>
   );
 };
