19d8a0c4e9271725cd55dc88110550a021e408af
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-05-17 00:56:52 +0530

    portfolio-responsive

--- src/Pages/Dashboard/Components/PortfolioSection.jsx
+++ src/Pages/Dashboard/Components/PortfolioSection.jsx
@@ -7,25 +7,73 @@ import { PiHandDepositFill } from "react-icons/pi";
 
 const PortfolioSection = () => {
   return (
-    <HStack bg="white" border="xl" p="6" spacing={16} justify={"space-between"}>
-      <HStack>
+    <HStack
+      bg="white"
+      border="xl"
+      p="6"
+      justify={"space-between"}
+      flexDir={{
+        base: "column",
+        xl: "row",
+      }}
+      align={{
+        base: "flex-start",
+        xl: "center",
+      }}
+      spacing={{
+        base: 4,
+        xl: 0,
+      }}
+    >
+      <HStack
+        spacing={{
+          base: "4",
+          xl: "16",
+        }}
+        flexDir={{
+          base: "column",
+          xl: "row",
+        }}
+        align={{
+          base: "flex-start",
+          xl: "center",
+        }}
+      >
         <Stack>
           <HStack color="black.80">
             <Text fontSize="sm">Total Portfolio Value</Text>
             <Icon as={AiOutlineInfoCircle} />
           </HStack>
-          <Text textStyle="h2">₹ 114,525.53</Text>
+          <Text textStyle="h2" fontWeight="medium">
+            ₹ 114,525.53
+          </Text>
         </Stack>
         <Stack>
           <HStack color="black.80">
             <Text fontSize="sm">Wallet Balances</Text>
           </HStack>
-          <HStack>
+          <HStack
+            spacing={4}
+            flexDir={{
+              base: "column",
+              sm: "row",
+            }}
+            align={{
+              base: "flex-start",
+              sm: "center",
+            }}
+          >
             <HStack>
-              <Text textStyle="h2">22.39047815</Text> <Tag>BTC</Tag>
+              <Text textStyle="h2" fontWeight="medium">
+                22.39047815
+              </Text>{" "}
+              <Tag colorScheme="gray">BTC</Tag>
             </HStack>
             <HStack>
-              <Text textStyle="h2">₹ 1,300.00</Text> <Tag>INR</Tag>
+              <Text textStyle="h2" fontWeight="medium">
+                ₹ 1,300.00
+              </Text>{" "}
+              <Tag colorScheme="gray">INR</Tag>
             </HStack>
           </HStack>
         </Stack>
