6747ffe188485e2f06758b2061726b00cd1aee5e
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-05-18 21:42:21 +0530

    Link-page routing

--- src/Components/Sidenav.jsx
+++ src/Components/Sidenav.jsx
@@ -1,10 +1,20 @@
-import { Box, HStack, Heading, Icon, Link, Stack, Text } from "@chakra-ui/react";
+import { Box, HStack, Heading, Icon,  Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import { RxDashboard } from "react-icons/rx";
 import { BsArrowDownUp } from "react-icons/bs";
 import { BiSupport } from "react-icons/bi";
+import { Link, useLocation } from "react-router-dom";
 
 const Sidenav = () => {
+
+  const location = useLocation();
+
+  console.log(location);
+
+  const isActiveLink = (link) => {
+    return location.pathname === link;
+  };
+
   const navLinks = [
     {
       icon: RxDashboard,
@@ -39,6 +49,7 @@ const Sidenav = () => {
         {/* 24/6 */}
         <Box mt="6" mx="12px">
           {navLinks.map((nav) => (
+            <Link to ={nav.link} key={nav.text}>
             <HStack
               py="3"
               px="4"
@@ -46,19 +57,21 @@ const Sidenav = () => {
                 bg: "#F3F3F7",
                 color: "#171717",
               }}
-              color="#797E82"
               borderRadius="10px"
-              key={nav.text}
+              bg={isActiveLink(nav.link) ? "#F3F3F7" : "transparent"}
+              color={isActiveLink(nav.link) ? "#171717" : "#797E82"}
             >
               <Icon as={nav.icon} />
               <Text fontSize="14px" fontWeight="medium">
                 {nav.text}
               </Text>
             </HStack>
+            </Link>
           ))}
         </Box>
       </Box>
       <Box mt="6" mx="12px" mb="6">
+        <Link to='/support'>
         <HStack
           py="3"
           px="4"
@@ -66,14 +79,16 @@ const Sidenav = () => {
             bg: "#F3F3F7",
             color: "#171717",
           }}
-          color="#797E82"
           borderRadius="10px"
+          bg={isActiveLink("/support") ? "#F3F3F7" : "transparent"}
+          color={isActiveLink("/support") ? "#171717" : "#797E82"}
         >
           <Icon as={BiSupport} />
             <Text fontSize="14px" fontWeight="medium">
             Support
           </Text>
         </HStack>
+        </Link>
       </Box>
     </Stack>
   );
